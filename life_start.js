41e315918ee061aca44a29c65c6809e97ee8af98
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-03-01 16:49:56 -0800

    add simple proxy, logger

--- life_start.js
+++ life_start.js
@@ -1,15 +1,22 @@
 var app = require('express').createServer(),
     jsdav = require('jsdav'),
     DavHandler = require('jsDAV/lib/DAV/handler').jsDAV_Handler,
-    FsTree = require('jsDAV/lib/DAV/tree/filesystem').jsDAV_Tree_Filesystem;
+    FsTree = require('jsDAV/lib/DAV/tree/filesystem').jsDAV_Tree_Filesystem,
+    log4js = require('log4js');
 
 var config = {
     "host": "localhost",
     "port": "4444",
     "srvOptions": {
       "node": "LivelyKernel/"
-    }
+    },
+    "logLevel": "debug"
   };
+
+var logger = log4js.getLogger();
+logger.setLevel(config.logLevel);
+
+var proxy = require('./lib/proxy')(logger);
 
 app.tree = new FsTree(config.srvOptions.node);
 
@@ -18,13 +25,26 @@ var fileHandler = function(req, resp) {
   new DavHandler(app, req, resp);
 };
 
+// Proxy routes
+app.get(/\/proxy\/(.*)/, function(req, res) {
+  proxy.get(req.params[0], req, res);
+}); 
+app.post(/\/proxy\/(.*)/, function(req, res) {
+  proxy.post(req.params[0], req, res);
+}); 
+app.put(/\/proxy\/(.*)/, function(req, res) {
+  proxy.put(req.params[0], req, res);
+}); 
 
+// DAV routes
 app.get(/.*/, fileHandler);
-
+app.put(/.*/, fileHandler);
+app.post(/.*/, fileHandler);
+app.delete(/.*/, fileHandler);
 app.propfind(/.*/, fileHandler);
 
-// FIXME add proxy route
 
+// GO GO GO
 app.listen(config.port);
 
 //jsdav.createServer(config.srvOptions, config.port, config.host);
