dabc46ffa94e31780ff4ed5b5cd779c24b28653b
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-03-01 16:11:39 -0800

    integrated expressjs with jsDAV

--- life_start.js
+++ life_start.js
@@ -1,4 +1,8 @@
-var jsdav = require('jsdav');
+var app = require('express').createServer(),
+    jsdav = require('jsdav'),
+    DavHandler = require('jsDAV/lib/DAV/handler').jsDAV_Handler,
+    FsTree = require('jsDAV/lib/DAV/tree/filesystem').jsDAV_Tree_Filesystem;
+
 var config = {
     "host": "localhost",
     "port": "4444",
@@ -7,4 +11,17 @@ var config = {
     }
   };
 
-jsdav.createServer(config.srvOptions, config.port, config.host);
+var tree = new FsTree(config.srvOptions.node);
+app.tree = tree;
+
+var fileHandler = function(req, resp) {
+  new DavHandler(app, req, resp);
+};
+
+app.get(/.*/, fileHandler);
+
+// FIXME add proxy route
+
+app.listen(config.port);
+
+//jsdav.createServer(config.srvOptions, config.port, config.host);
